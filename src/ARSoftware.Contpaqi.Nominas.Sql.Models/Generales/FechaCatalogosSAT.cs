9511331b0a70cf352bc74bc8a83b48acbcbd9153
Andres Ramos <andres@arsoft.net>
2023-04-25 12:19:11 -0500

    delete unused folder

--- src/ARSoftware.Contpaqi.Nominas.Sql.Models/Generales/FechaCatalogosSAT.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ARSoftware.Contpaqi.Nominas.Sql.Models.Generales;
-
-public partial class FechaCatalogosSAT
-{
-    public DateTime UltimaActualizacion { get; set; }
-}
--- src/ARSoftware.Contpaqi.Nominas.Sql.Models/Generales/VersionDiccionarioCatalogos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ARSoftware.Contpaqi.Nominas.Sql.Models.Generales;
-
-public partial class VersionDiccionarioCatalogos
-{
-    public int VersionActual { get; set; }
-}
