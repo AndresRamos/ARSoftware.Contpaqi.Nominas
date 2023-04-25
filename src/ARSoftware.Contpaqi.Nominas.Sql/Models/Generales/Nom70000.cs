a239b2c012b43afc344b91f8e66b3d283e808757
Andres Ramos <andres@arsoft.net>
2023-04-25 13:11:59 -0500

    scaffold generales database version 15.2.1

---
+++ src/ARSoftware.Contpaqi.Nominas.Sql/Models/Generales/Nom70000.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ARSoftware.Contpaqi.Nominas.Sql.Models.Generales;
+
+public partial class Nom70000
+{
+    public int idTabla { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public DateTime FechaVigencia { get; set; }
+
+    public byte TipoZona { get; set; }
+
+    public bool Reportada { get; set; }
+
+    public int Version { get; set; }
+
+    public DateTime TimeStamp { get; set; }
+}
---
+++ src/ARSoftware.Contpaqi.Nominas.Sql/Models/Generales/Nom90000.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ARSoftware.Contpaqi.Nominas.Sql.Models.Generales;
+
+public partial class Nom90000
+{
+    public int Id { get; set; }
+
+    public int Version { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public DateTime TimeStamp { get; set; }
+}
